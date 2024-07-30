339f7d6b480c7b54cbc77edcb963301072dc36c4
DucPhat <toanphat1909@gmail.com>
2024-07-30 13:51:31 +0700

    final commit

---
+++ AppCoffee/AppCoffee/CauHinh_QuanLyCaPhe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCoffee
+{
+    class CauHinh_QuanLyCaPhe
+    {
+        public static string connect = "Data Source=DESKTOP-TA0IF4R\\DPHAT;Initial Catalog=QLCaPhe_Official8;User ID=sa;Password=123456;MultipleActiveResultSets=True;";
+    }
+}
